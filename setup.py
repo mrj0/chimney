69ff888db27b9a28764e3574497eaadc92a435f5
Mike Johnson <m.johnson@evite.com>
2013-12-05 19:58:19 -0800

    update to alpha in trove

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url='https://github.com/mrj0/chimney/',
     license='BSD',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
     ],
     packages=['chimney'],
     install_requires=['six>=1.3', 'futures', 'watchdog'],
